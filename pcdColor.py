9678be2d646c34bca106d047b528369d0b6b8490
Fred Goo <fredera92@gmail.com>
2018-09-24 15:49:01 +0800

    Update: 存储点云

--- pcdColor.py
+++ pcdColor.py
@@ -20,8 +20,19 @@ class CameraIntrinsicParameters(object):
 
 # 点云展示
 def pcdViewer(filename):
-    cloud = pcl.load(filename)
+    cloud = pcl.load_XYZRGB(filename)
     print("cloud points : " + str(cloud.size))
+    visual = pcl.pcl_visualization.CloudViewing()
+
+    # PointXYZ
+    # visual.ShowMonochromeCloud(cloud)
+
+    visual.ShowColorCloud(cloud, b'cloud')
+
+    flag = True
+    while flag:
+        flag != visual.WasStopped()
+    end
 
 
 # 图片转点云
@@ -49,7 +60,6 @@ def AddColorToPCDFile(filename):
 def imageToPointCloud(RGBFilename, DepthFilename, CloudFilename, camera):
     rgb = cv2.imread(RGBFilename)
     depth = cv2.imread(DepthFilename, cv2.COLOR_BGR2GRAY)
-    print(depth)
     # ROS中rqt保存的深度摄像头的图片是rgb格式，需要转换成单通道灰度格式
     if len(depth[0][0]) == 3:
         depth = cv2.cvtColor(depth, cv2.COLOR_BGR2GRAY)
@@ -77,7 +87,7 @@ def imageToPointCloud(RGBFilename, DepthFilename, CloudFilename, camera):
     pcl.save(cloud, CloudFilename, format='pcd')
 
 
-# pcdViewer('./pcd/cloud.pcd')
+pcdViewer('./pcd/cloud.pcd')
 camera = CameraIntrinsicParameters(3.6927587384670619e+02, 2.0049685183455608e+02,
                                    6.0782982475382448e+02, 6.0782982475382448e+02, 1000.0)
-imageToPointCloud('./slam/rgb.png', './slam/depth.png', './pcd/cloud.pcd', camera)
+# imageToPointCloud('slam/rgb.png', 'slam/depth.png', 'pcd/cloud.pcd', camera)
