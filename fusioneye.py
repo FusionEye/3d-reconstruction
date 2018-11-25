9450a59946a03401eeb6b57886a90827cf55b5f7
Fred Goo <fredera92@gmail.com>
2018-11-26 07:27:12 +0800

    ignore python-pcl

--- fusioneye.py
+++ fusioneye.py
@@ -2,7 +2,7 @@
 
 import sys
 from util.image_util import split_ricoh_images
-# from lidar.hokuyo.pcl_tools import merge_point_cloud_to_pcd
+from lidar.hokuyo.pcl_tools import merge_point_cloud_to_pcd
 from lidar.color_point_cloud import color_pcd
 
 if __name__ == '__main__':
@@ -13,11 +13,11 @@ if __name__ == '__main__':
         split_ricoh_images(sys.argv[2], sys.argv[3])
 
     # 点云合成
-    # if sys.argv[1] == 'merge_pcd':
-    #     if argvLen == 2:
-    #         merge_point_cloud_to_pcd('./input/pointCloud/', './input/pointCloud.pcd')
-    #     if argvLen == 4:
-    #         merge_point_cloud_to_pcd(sys.argv[2], sys.argv[3])
+    if sys.argv[1] == 'merge_pcd':
+        if argvLen == 2:
+            merge_point_cloud_to_pcd('./input/pointCloud/', './input/pointCloud.pcd')
+        if argvLen == 4:
+            merge_point_cloud_to_pcd(sys.argv[2], sys.argv[3])
 
     # 点云上色
     if sys.argv[1] == 'color_pcd':
